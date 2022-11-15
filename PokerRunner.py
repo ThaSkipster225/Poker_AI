f9770cdea9843994c9262ed4bdd7a98e1bd28c81
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-15 15:10:14 -0500

    Got and stored data

--- PokerRunner.py
+++ PokerRunner.py
@@ -1,2 +1,11 @@
 # This file will allow you to choose which of the 
 # other Python files you want to run from this repository.
+
+import numpy as np
+
+data = np.loadtxt('poker-hand-training.csv', dtype=int, delimiter=',')
+
+x = data[:, :10]
+labels = data[:, 10]
+
+print(labels)
