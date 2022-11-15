343bf967a9922a3dbac8f7fa5a540225563b2aa7
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-15 16:02:34 -0500

    Added necessary Stuff

--- PokerRunner.py
+++ PokerRunner.py
@@ -1,11 +1,25 @@
-# This file will allow you to choose which of the 
-# other Python files you want to run from this repository.
+# This file serves as the template for the other ones,
+# it has the basics you would need for the other ones 
+# and to use the algorithms we set out to use.
 
+# Import Section
 import numpy as np
+import os
 
-data = np.loadtxt('poker-hand-training.csv', dtype=int, delimiter=',')
+# Define the main function.
+def main():
+    # Define our constants
+    ROOT = os.path.dirname(os.path.abspath(__file__)) # root directory of this file
+    TRAININGDATAFILE = 'poker-hand-training.csv'
+    TESTDATAFILE = 'poker-hand-testing.csv'
+    
+    # Extract the data by using loadtxt and store it in the variable training_data.
+    training_data = np.loadtxt(TRAININGDATAFILE, dtype=int, delimiter=',')
 
-x = data[:, :10]
-labels = data[:, 10]
+    # Splice the array of data to separate the labels from the rest of the data.
+    train_x = training_data[:, :10]
+    train_labels = training_data[:, 10]
 
-print(labels)
+
+if __name__ == '__main__':
+    main()
