1c8cabedf10683a5fe1fdf6030cc1cddc4687e5f
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-12-10 14:30:19 -0500

    Optimizing the SVM
    Working on optimizing the SVM to train quicker and better.

--- PokerSVM.py
+++ PokerSVM.py
@@ -24,13 +24,24 @@ def main():
     train_labels = training_data[:, 10]
     test_x = testing_data[:, :10]
     test_labels = testing_data[:,10]
+    numoutputs = len(np.unique(train_labels))
 
     # Training
-    clf = svm.SVC()
+    clf = svm.SVC(
+        kernel='poly'
+        , C=0.1
+        , decision_function_shape='ovr'
+        , degree=numoutputs
+        )
+
+    print('Beginning Training')
     clf.fit(train_x, train_labels)
+    print('Training Ended')
 
     # Predict
+    print('Beginning Testing')
     classifer_pred = clf.predict(test_x)
+    print('Testing ended')
 
     # Get accuracy
     print(f'{accuracy_score(test_labels, classifer_pred)*100}% accurate')
