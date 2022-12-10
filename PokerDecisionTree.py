8440ccc2a560b9cdf89dfb66aa07641d9205e88f
anselmiscolumna <57639912+anselmiscolumna@users.noreply.github.com>
2022-12-10 17:26:27 -0500

    Created initial decision tree file

---
+++ PokerDecisionTree.py
@@ -0,0 +1,30 @@
+# This file serves as the template for the other ones,
+# it has the basics you would need for the other ones 
+# and to use the algorithms we set out to use.
+
+# Import Section
+import numpy as np
+import os
+
+# Define the main function.
+def main():
+    # Define our constants
+    ROOT = os.path.dirname(os.path.abspath(__file__)) # root directory of this file
+    TRAININGDATAFILE = 'poker-hand-training.csv'
+    TESTDATAFILE = 'poker-hand-testing.csv'
+    
+    # Extract the data by using loadtxt and store it in the variable training_data.
+    training_data = np.loadtxt(TRAININGDATAFILE, dtype=int, delimiter=',')
+    testing_data = np.loadtxt(TESTDATAFILE, dtype=int, delimiter=',')
+
+    # Splice the array of data to separate the labels from the rest of the data.
+    train_x = training_data[:, :10]
+    train_labels = training_data[:, 10]
+    test_x = testing_data[:, :10]
+    test_labels = testing_data[:,10]
+
+    
+
+
+if __name__ == '__main__':
+    main()
